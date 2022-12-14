ab7aee80d4486bca749f76543ff0aa4253d71676
JohnWL <34081873+John-WL@users.noreply.github.com>
2022-12-14 02:27:30 -0500

    refactored postprocess

--- scripts/auto_backup.py
+++ scripts/auto_backup.py
@@ -12,6 +12,24 @@ sys.path.append(base_dir)
 
 config_path = os.path.join(base_dir, 'user-config.cfg')
 
+
+def make_missing_directory_and_parent(destination_directory):
+    if not os.path.exists(destination_directory):
+        parent_directory = os.path.dirname(destination_directory)
+        os.mkdir(parent_directory)
+    os.mkdir(destination_directory)
+
+
+def save_images_to_directory(images, backup_directory):
+    for image in images:
+        full_destination = os.path.join(backup_directory, image.already_saved_as)
+        destination_directory = os.path.dirname(full_destination)
+        if not os.path.exists(destination_directory):
+            make_missing_directory_and_parent(destination_directory)
+        image.save(full_destination)
+        print('saved "\033[94m' + os.path.basename(full_destination) + '\033[0m" to "\033[94m' + os.path.dirname(full_destination) + '\033[0m"')
+
+
 class AutoBackupScript(scripts.Script):
     def __init__(self):
         self.config = configparser.ConfigParser()
@@ -36,16 +54,7 @@ class AutoBackupScript(scripts.Script):
         save_paths = args[0].split('|')
         if opts.samples_save and not p.do_not_save_samples:
             for save_path in save_paths:
-                for image in res.images:
-                    save_path = save_path.strip()
-                    if save_path == '':
-                        continue
-                    save_path = os.path.join(save_path, image.already_saved_as)
-                    if not os.path.exists(os.path.dirname(save_path)):
-                        dir_to_make = os.path.dirname(save_path)
-                        if not os.path.exists(dir_to_make):
-                            root_dir_to_make = os.path.dirname(dir_to_make)
-                            os.mkdir(root_dir_to_make)
-                        os.mkdir(dir_to_make)
-                    image.save(save_path)
-                    print('saved "\033[94m' + os.path.basename(save_path) + '\033[0m" to "\033[94m' + os.path.dirname(save_path) + '\033[0m"')
+                save_path = save_path.strip()
+                if save_path == '':
+                    continue
+                save_images_to_directory(res.images, save_path)
